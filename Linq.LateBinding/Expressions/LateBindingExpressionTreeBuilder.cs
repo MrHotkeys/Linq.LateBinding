0772ace4a41999c6dfd38bb987f7074c0501f4e4
Mr.Hotkeys <mr.hotkeys@send.help>
2022-07-10 16:48:37 -0400

    Add MemberOverrides to LateBindingExpressionTreeBuilder

--- Linq.LateBinding/Expressions/LateBindingExpressionTreeBuilder.cs
+++ Linq.LateBinding/Expressions/LateBindingExpressionTreeBuilder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -7,11 +8,56 @@ namespace MrHotkeys.Linq.LateBinding.Expressions
 {
     public sealed class LateBindingExpressionTreeBuilder
     {
+        private Dictionary<MemberInfo, MemberOverrideDefinition> MemberOverrides { get; set; } = new Dictionary<MemberInfo, MemberOverrideDefinition>();
+
         private CalculateExpressionManager CalculateExpressionManager { get; }
 
         public LateBindingExpressionTreeBuilder(CalculateExpressionManager calculateExpressionManager)
         {
             CalculateExpressionManager = calculateExpressionManager ?? throw new ArgumentNullException(nameof(calculateExpressionManager));
+        }
+
+        public LateBindingExpressionTreeBuilder DefineMemberOverride(MemberInfo member, Func<Expression, Expression> buildOverrideFunc, bool onlyOnDirect)
+        {
+            if (member is null)
+                throw new ArgumentNullException(nameof(member));
+            if (buildOverrideFunc is null)
+                throw new ArgumentNullException(nameof(buildOverrideFunc));
+            if (member.MemberType != MemberTypes.Field && member.MemberType != MemberTypes.Property)
+                throw new ArgumentException("Must be a field or property!", nameof(member));
+
+            MemberOverrides[member] = new MemberOverrideDefinition(member, buildOverrideFunc, onlyOnDirect);
+
+            return this;
+        }
+
+        public LateBindingExpressionTreeBuilder DefineMemberOverride<TTarget, TMember, TOverride>(
+            Expression<Func<TTarget, TMember>> memberGetterExpr, Expression<Func<TTarget, TOverride>> overrideExpr, bool onlyOnDirect)
+        {
+            if (memberGetterExpr is null)
+                throw new ArgumentNullException(nameof(memberGetterExpr));
+            if (overrideExpr is null)
+                throw new ArgumentNullException(nameof(overrideExpr));
+
+            if (memberGetterExpr.Body is not MemberExpression memberExpr)
+                throw new ArgumentException("Must be a simple field or property getter (e.g. (User u) => u.Email)!", nameof(memberGetterExpr));
+            if (memberExpr.Expression != memberGetterExpr.Parameters[0])
+                throw new ArgumentException("The field or property must be coming from the parameter (e.g. (User u) => u.Email)!", nameof(memberGetterExpr));
+
+            var member = memberExpr.Member;
+            if (member.MemberType != MemberTypes.Field && member.MemberType != MemberTypes.Property)
+                throw new ArgumentException("Must be getting a field or property!", nameof(memberGetterExpr));
+
+            var buildOverrideFunc = (Expression expression) =>
+            {
+                var visitor = new ParameterExpressionReplaceVisitor();
+                visitor.Add(overrideExpr.Parameters[0], expression);
+                return visitor.Visit(overrideExpr.Body);
+            };
+
+            MemberOverrides[member] = new MemberOverrideDefinition(member, buildOverrideFunc, onlyOnDirect);
+
+            return this;
         }
 
         public Expression Build(Expression targetExpr, ILateBindingExpression lateBinding)
@@ -45,15 +91,23 @@ namespace MrHotkeys.Linq.LateBinding.Expressions
                 .Field
                 .Split(".");
             var currentExpr = targetExpr;
-            foreach (var field in split)
+            for (var i = 0; i < split.Length; i++)
             {
+                var field = split[i];
                 var property = currentExpr
                     .Type
                     .GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
                     .Where(p => StringComparer.OrdinalIgnoreCase.Equals(p.Name, field))
                     .Single();
 
-                currentExpr = Expression.MakeMemberAccess(currentExpr, property);
+                if (MemberOverrides.TryGetValue(property, out var memberOverride) && (memberOverride.OnlyOnDirect == false || i == split.Length - 1))
+                {
+                    currentExpr = memberOverride.BuildOverride(currentExpr);
+                }
+                else
+                {
+                    currentExpr = Expression.MakeMemberAccess(currentExpr, property);
+                }
             }
 
             return currentExpr;
@@ -67,6 +121,24 @@ namespace MrHotkeys.Linq.LateBinding.Expressions
                 .ToArray();
 
             return CalculateExpressionManager.Build(calculateLateBinding.Method, expressions);
+        }
+
+        private sealed class MemberOverrideDefinition
+        {
+            public MemberInfo Member { get; }
+
+            private Func<Expression, Expression> BuildOverrideFunc { get; }
+
+            public bool OnlyOnDirect { get; }
+
+            public MemberOverrideDefinition(MemberInfo member, Func<Expression, Expression> buildOverrideFunc, bool onlyOnDirect)
+            {
+                Member = member ?? throw new ArgumentNullException(nameof(member));
+                BuildOverrideFunc = buildOverrideFunc ?? throw new ArgumentNullException(nameof(buildOverrideFunc));
+                OnlyOnDirect = onlyOnDirect;
+            }
+
+            public Expression BuildOverride(Expression expr) => BuildOverrideFunc(expr);
         }
     }
 }
