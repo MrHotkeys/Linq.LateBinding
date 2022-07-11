0de4e33f975c9c645ece7b915e7516357b5cd74a
Mr.Hotkeys <mr.hotkeys@send.help>
2022-07-11 08:10:13 -0400

    Helper methods for working with IEnumerable<T> Expressions

---
+++ Linq.LateBinding/LateBindingHelpers.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace MrHotkeys.Linq.LateBinding
+{
+    public static class LateBindingHelpers
+    {
+        public static IEnumerable<Type> GetIEnumerableInterfaces(Type type, bool genericOnly)
+        {
+            if (type is null)
+                throw new ArgumentNullException(nameof(type));
+
+            var interfaces = type
+                .GetInterfaces()
+                .AsEnumerable();
+            if (type.IsInterface)
+                interfaces = interfaces.Append(type);
+
+            return interfaces.Where(i => i.Name.StartsWith(nameof(IEnumerable)) && (!genericOnly || i.IsGenericType));
+        }
+
+        public static MethodInfo GetIEnumerableMethod(LambdaExpression expr, params Type[] typeArgs)
+        {
+            if (expr is null)
+                throw new ArgumentNullException(nameof(expr));
+            if (expr.Body is not MethodCallExpression callExpr)
+                throw new ArgumentException();
+
+            var method = callExpr.Method;
+
+            if (typeArgs?.Length > 0)
+            {
+                method = method
+                    .GetGenericMethodDefinition()
+                    .MakeGenericMethod(typeArgs);
+            }
+
+            return method;
+        }
+    }
+}
