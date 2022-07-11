884b9626f6b6a5545493dc4c2e19af681701b1dc
Mr.Hotkeys <mr.hotkeys@send.help>
2022-07-10 23:24:06 -0400

    Add QueryableWithLateBinding<T>.Where

--- Linq.LateBinding/QueryableWithLateBinding{T}.cs
+++ Linq.LateBinding/QueryableWithLateBinding{T}.cs
@@ -24,12 +24,10 @@ namespace MrHotkeys.Linq.LateBinding
 
         public QueryableWithLateBinding<object?> Query(ILateBindingQuery query)
         {
-            var queryable = query.Select is not null ?
-                Select(query.Select) :
-                SelectAsObjects();
+            var queryable = this;
 
             if (query.Where is not null)
-            { }
+                queryable = queryable.Where(query.Where);
 
             if (query.OrderBy is not null)
             { }
@@ -40,7 +38,76 @@ namespace MrHotkeys.Linq.LateBinding
             if (query.Take.HasValue)
             { }
 
-            return queryable;
+            // Select last since it upcasts everything as object (since the result will be a generated DTO type if a select
+            // is defined) and that will prevent Where and OrderBy from seeing the correct object type to find members
+            return query.Select is not null ?
+                queryable.Select(query.Select) :
+                queryable.SelectAsObjects();
+        }
+
+        public QueryableWithLateBinding<object?> Select(IDictionary<string, ILateBindingExpression> select)
+        {
+            if (select is null)
+                throw new ArgumentNullException(nameof(select));
+
+            var selectSourceParameterExpr = Expression.Parameter(typeof(T));
+            var selectMemberExpressions = new Dictionary<string, Expression>();
+
+            var dtoPropertyDefinitions = new List<DtoPropertyDefinition>();
+
+            foreach (var (name, lateBindingExpression) in select)
+            {
+                var expression = QueryableWithLateBinding.ExpressionTreeBuilder.Build(selectSourceParameterExpr, lateBindingExpression);
+                selectMemberExpressions[name] = expression;
+
+                var dtoPropertyDefinition = new DtoPropertyDefinition(name, expression.Type);
+                dtoPropertyDefinitions.Add(dtoPropertyDefinition);
+            }
+
+            var dtoType = QueryableWithLateBinding.DtoGenerator.Generate(dtoPropertyDefinitions);
+
+            var dtoConstructor = dtoType.GetConstructor(BindingFlags.Public | BindingFlags.Instance, Type.DefaultBinder, Type.EmptyTypes, null) ?? throw new InvalidOperationException();
+
+            var selectMemberBindings = new List<MemberBinding>();
+            foreach (var (name, expression) in selectMemberExpressions)
+            {
+                var property = dtoType.GetProperty(name) ?? throw new InvalidOperationException();
+                var memberBinding = Expression.Bind(property, expression);
+                selectMemberBindings.Add(memberBinding);
+            }
+
+            var selectNewExpr = Expression.New(dtoConstructor);
+            var selectMemberInitExpr = Expression.MemberInit(selectNewExpr, selectMemberBindings);
+            var selectObjectExpr = Expression.Convert(selectMemberInitExpr, typeof(object));
+            var selectExpr = Expression.Lambda<Func<T, object>>(selectMemberInitExpr, selectSourceParameterExpr); // TODO: Add null for source parameter
+
+            var entitiesSelected = Entities.Select(selectExpr);
+            return new QueryableWithLateBinding<object?>(entitiesSelected);
+        }
+
+        private QueryableWithLateBinding<object?> SelectAsObjects()
+        {
+            var entitiesSelected = Entities.Select(x => (object?)x);
+            return new QueryableWithLateBinding<object?>(entitiesSelected);
+        }
+
+        public QueryableWithLateBinding<T> Where(IEnumerable<ILateBindingExpression> where)
+        {
+            var whereTargetParameterExpr = Expression.Parameter(typeof(T));
+            var whereBodyExpr = where
+                .Select(w =>
+                {
+                    var expression = QueryableWithLateBinding.ExpressionTreeBuilder.Build(whereTargetParameterExpr, w);
+                    if (expression.Type != typeof(bool))
+                        throw new InvalidOperationException();
+                    return expression;
+                })
+                .Aggregate((left, right) => Expression.And(left, right));
+
+            var whereExpr = Expression.Lambda<Func<T, bool>>(whereBodyExpr, whereTargetParameterExpr);
+
+            var entitiesSelected = Entities.Where(whereExpr);
+            return new QueryableWithLateBinding<T>(entitiesSelected);
         }
 
         public QueryableWithLateBinding<T> OrderBy(string propertyName) =>
@@ -78,52 +145,6 @@ namespace MrHotkeys.Linq.LateBinding
                 Entities.OrderByDescending(lambdaExpr);
 
             return new QueryableWithLateBinding<T>(entitiesOrdered);
-        }
-
-        public QueryableWithLateBinding<object?> Select(IDictionary<string, ILateBindingExpression> select)
-        {
-            if (select is null)
-                throw new ArgumentNullException(nameof(select));
-
-            var selectSourceParameterExpr = Expression.Parameter(typeof(T));
-            var selectMemberExpressions = new Dictionary<string, Expression>();
-
-            var dtoPropertyDefinitions = new List<DtoPropertyDefinition>();
-
-            foreach (var (name, lateBindingExpression) in select)
-            {
-                var expression = QueryableWithLateBinding.ExpressionTreeBuilder.Build(selectSourceParameterExpr, lateBindingExpression);
-                selectMemberExpressions[name] = expression;
-
-                var dtoPropertyDefinition = new DtoPropertyDefinition(name, expression.Type);
-                dtoPropertyDefinitions.Add(dtoPropertyDefinition);
-            }
-
-            var dtoType = QueryableWithLateBinding.DtoGenerator.Generate(dtoPropertyDefinitions);
-
-            var dtoConstructor = dtoType.GetConstructor(BindingFlags.Public | BindingFlags.Instance, Type.DefaultBinder, Type.EmptyTypes, null);
-
-            var selectMemberBindings = new List<MemberBinding>();
-            foreach (var (name, expression) in selectMemberExpressions)
-            {
-                var property = dtoType.GetProperty(name) ?? throw new InvalidOperationException();
-                var memberBinding = Expression.Bind(property, expression);
-                selectMemberBindings.Add(memberBinding);
-            }
-
-            var selectNewExpr = Expression.New(dtoConstructor);
-            var selectMemberInitExpr = Expression.MemberInit(selectNewExpr, selectMemberBindings);
-            var selectObjectExpr = Expression.Convert(selectMemberInitExpr, typeof(object));
-            var selectExpr = Expression.Lambda<Func<T, object>>(selectMemberInitExpr, selectSourceParameterExpr); // TODO: Add null for source parameter
-
-            var entitiesSelected = Entities.Select(selectExpr);
-            return new QueryableWithLateBinding<object?>(entitiesSelected);
-        }
-
-        private QueryableWithLateBinding<object?> SelectAsObjects()
-        {
-            var entitiesSelected = Entities.Select(x => (object?)x);
-            return new QueryableWithLateBinding<object?>(entitiesSelected);
         }
 
         public IEnumerator<T> GetEnumerator() => Entities.GetEnumerator();
