1f3f7cd051802ed0e178aec95622d29cfc8b4aeb
Mr.Hotkeys <mr.hotkeys@send.help>
2022-07-10 23:52:19 -0400

    LateBindingExtensions -> LinqExtensions, add overloads that take ILateBindingQuery

--- Linq.LateBinding/LinqExtensions.cs
+++ Linq.LateBinding/LinqExtensions.cs
@@ -4,13 +4,18 @@ using MrHotkeys.Linq.LateBinding;
 
 namespace System.Linq
 {
-    public static class LinqExtensions
+    public static class LateBindingExtensions
     {
-        public static QueryableWithLateBinding<T> WithLateBinding<T>(this IQueryable<T> entities) => new QueryableWithLateBinding<T>(entities);
-    }
+        public static QueryableWithLateBinding<T> WithLateBinding<T>(this IQueryable<T> entities) =>
+            new QueryableWithLateBinding<T>(entities);
+        public static QueryableWithLateBinding<object?> WithLateBinding<T>(this IQueryable<T> entities, ILateBindingQuery query) =>
+            new QueryableWithLateBinding<T>(entities)
+                .Query(query);
 
-    public static class IEnumerableExtensions
-    {
-        public static QueryableWithLateBinding<T> AsQueryableWithLateBinding<T>(this IEnumerable<T> entities) => new QueryableWithLateBinding<T>(entities.AsQueryable());
+        public static QueryableWithLateBinding<T> AsQueryableWithLateBinding<T>(this IEnumerable<T> entities) =>
+            new QueryableWithLateBinding<T>(entities.AsQueryable());
+        public static QueryableWithLateBinding<object?> AsQueryableWithLateBinding<T>(this IEnumerable<T> entities, ILateBindingQuery query) =>
+            new QueryableWithLateBinding<T>(entities.AsQueryable())
+                .Query(query);
     }
 }
